e246f052b53488c1086820cb47b6adc758668039
Quy <suthanbien@gmail.com>
2020-08-17 21:02:22 +0700

    Thanh Quy up bai tap 6

---
+++ main.java
@@ -0,0 +1,12 @@
+package run;
+
+public class main {
+public static void main(String[] args) {
+	
+	  System.out.println("Social-Original-SO ");
+	  System.out.println("Họ và tên : Trần Thanh Quy ");
+	  System.out.println("Email : thanhquy0164@gmail.com");
+	  System.out.println(" sdt : 0375250024");
+	  System.out.println("ocial : https://www.facebook.com/thanhquy99/");
+}
+}
