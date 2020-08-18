6220f2f4ebc1663bffda874fdbd493ae3050a418
unknown <khanguyen1000@gmail.com>
2020-08-18 21:09:51 +0700

    Nguyen ngoc kha hello word

--- main.java
+++ main.java
@@ -4,9 +4,9 @@ public class main {
 public static void main(String[] args) {
 	
 	  System.out.println("Social-Original-SO ");
-	  System.out.println("Họ và tên : Trần Thanh Quy ");
-	  System.out.println("Email : thanhquy0164@gmail.com");
-	  System.out.println(" sdt : 0375250024");
+	  System.out.println("Họ và tên : Nguyễn Ngọc Kha");
+	  System.out.println("Email : Khanguyen1000@gmail.com");
+	  System.out.println(" sdt : 0329457486");
 	  System.out.println("ocial : https://www.facebook.com/thanhquy99/");
 }
 }
